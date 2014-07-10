c6d78d1f3001260f7983cc2b0daad62976cee17f
tko <tko@tko-macbookpro.roam.corp.google.com>
2014-07-10 12:02:49 -0700

    Hamburger

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hamburger
+//
+//  Created by Taeho Ko on 7/10/14.
+//  Copyright (c) 2014 google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
